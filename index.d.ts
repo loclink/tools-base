8620338ee115b020bdf28918b3df758aec75c4f1
loclink <loclink@163.com>
2022-07-18 09:46:35 +0800

    🦄 refactor: 配置修改为ts文件

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module '@rollup/plugin-node-resolve';
+declare module '@rollup/plugin-babel';
+declare module '@rollup/plugin-commonjs';
+declare module '@rollup/plugin-typescript';
+declare module 'rollup-plugin-terser';
